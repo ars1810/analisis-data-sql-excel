b32b7a661a9029f27be6b66eb4843ed9d273982e
ars1810 <122894254+ars1810@users.noreply.github.com>
2025-03-27 13:31:45 +0700

    Add files via upload

---
+++ src/sales_data.sql
@@ -0,0 +1,198 @@
+
+CREATE TABLE sales_data (
+    id SERIAL PRIMARY KEY,
+    product VARCHAR(255),
+    transaction_count INT,
+    total_sales DECIMAL(10,2),
+    avg_sales DECIMAL(10,2),
+    highest_price DECIMAL(10,2),
+    lowest_price DECIMAL(10,2)
+);
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Smartphone iPhone', 410.0, 3400.0, 8.29, 1133.33, 666.43);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Smart TV Samsung', 47.0, 1733.33, 36.88, 866.67, 509.62);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Smartphone Samsung', 120.0, 1700.0, 14.17, 566.67, 333.21);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Laptop ASUS', 464.0, 1600.0, 3.45, 800.0, 470.42);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Laptop MSI', 445.0, 1466.67, 3.3, 1466.67, 862.43);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Laptop Lenovo', 417.0, 1466.67, 3.52, 733.33, 431.22);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Camera Canon', 402.0, 1200.0, 2.99, 600.0, 352.81);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Gaming Console Xbox Series X', 115.0, 1133.33, 9.86, 566.67, 333.21);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Bicycle Polygon', 274.0, 1066.67, 3.89, 533.33, 313.61);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Electric Scooter Xiaomi', 218.0, 1000.0, 4.59, 333.33, 196.01);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('E-Bike United', 325.0, 1000.0, 3.08, 1000.0, 588.02);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Tablet Apple', 463.0, 1000.0, 2.16, 1000.0, 588.02);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('VR Headset Oculus', 446.0, 933.33, 2.09, 466.67, 274.41);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('TV LG', 57.0, 933.33, 16.37, 466.67, 274.41);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Earbuds Apple AirPods', 474.0, 700.0, 1.48, 233.33, 137.21);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Smart Thermostat Nest', 406.0, 666.67, 1.64, 333.33, 196.01);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Washing Machine LG', 253.0, 666.67, 2.64, 333.33, 196.01);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Headphone Sony', 402.0, 666.67, 1.66, 166.67, 98.0);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Home Theater Sony', 124.0, 666.67, 5.38, 666.67, 392.02);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Smartwatch Garmin', 383.0, 666.67, 1.74, 333.33, 196.01);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Tablet Samsung', 189.0, 600.0, 3.17, 600.0, 352.81);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Robot Vacuum iRobot', 436.0, 600.0, 1.38, 600.0, 352.81);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Fitness Tracker Fitbit', 232.0, 600.0, 2.59, 200.0, 117.6);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Gaming Console PlayStation 5', 379.0, 600.0, 1.58, 600.0, 352.81);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Bluetooth Speaker JBL', 198.0, 533.33, 2.69, 133.33, 78.4);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Refrigerator Samsung', 143.0, 533.33, 3.73, 533.33, 313.61);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Smart Door Lock Xiaomi', 465.0, 533.33, 1.15, 266.67, 156.81);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Smart Light Philips Hue', 190.0, 500.0, 2.63, 100.0, 58.8);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('GPU NVIDIA RTX 3050 ', 159.0, 500.0, 3.14, 500.0, 294.01);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Projector Epson', 69.0, 466.67, 6.76, 466.67, 274.41);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Tripod Manfrotto', 253.0, 400.0, 1.58, 133.33, 78.4);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Monitor Dell', 412.0, 400.0, 0.97, 200.0, 117.6);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Air Conditioner Daikin', 344.0, 400.0, 1.16, 400.0, 235.21);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Microwave Panasonic', 209.0, 400.0, 1.91, 133.33, 78.4);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('External HDD WD 4TB', 186.0, 400.0, 2.15, 133.33, 78.4);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('SSD Samsung 1TB', 176.0, 360.0, 2.05, 120.0, 70.56);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Hard Drive Seagate 2TB', 491.0, 320.0, 0.65, 80.0, 47.04);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Blender Philips', 208.0, 266.67, 1.28, 66.67, 39.2);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Printer Epson', 471.0, 266.67, 0.57, 266.67, 156.81);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Power Supply Corsair 750W', 78.0, 266.67, 3.42, 133.33, 78.4);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Router TP-Link', 155.0, 200.0, 1.29, 100.0, 58.8);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Gaming Mouse Logitech', 265.0, 200.0, 0.75, 66.67, 39.2);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Mechanical Keyboard Keychron', 212.0, 200.0, 0.94, 100.0, 58.8);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('USB Flash Drive SanDisk 128GB', 16.0, 200.0, 12.5, 20.0, 11.76);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('RAM Corsair 16GB', 403.0, 186.67, 0.46, 93.33, 54.88);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Keyboard Logitech', 338.0, 166.67, 0.49, 33.33, 19.6);
+    
+
+    INSERT INTO sales_data (product, transaction_count, total_sales, avg_sales, highest_price, lowest_price)
+    VALUES ('Mouse Razer', 396.0, 150.0, 0.38, 50.0, 29.4);
+    
